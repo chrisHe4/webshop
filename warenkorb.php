2858391a39c9af5aad3858a530eb1e351271793f
Jessi2601 <jessica.hirsch@condero.com>
2022-01-16 16:28:57 +0100

    navbar in allen Datein rausgenommen und über include reingeholt

--- warenkorb.php
+++ warenkorb.php
@@ -33,58 +33,9 @@ if(isset($_POST["produktID"])) {
     <title>webshop</title>
   </head>
 <body>
-  <!--NAVBAR--START--------------------------------------------------------------------------->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Webshop</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Achtung! form class d-flex macht rechtbündig-->
-        <form class="d-flex">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">   
-              <a class="nav-link active" href="#">Katalog</a></li>
-              <li><a class="nav-link active" href="warenkorb.html">Warenkorb <?php echo "(".count($_SESSION["warenkorb"]).")" ?></a></li>
-              <li><a class="nav-link active" href="#">Datenschutz</a></li>
-              <li><a class="nav-link active" href="#">Impressum</a> </li>
-            
-          <!--------------------------------
-            <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-            </li>
-          --------------------------------->
-          <!--------------------------------
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          ----------------------------------->
-            <div class="btn-group">
-              <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-                Anmelden
-              </button>
-              <ul class="dropdown-menu dropdown-menu-lg-end">
-                <li><a class="dropdown-item" href="login.html">Login</a></li>
-                <li><a class="dropdown-item" href="registrieren.html">Registrieren</a></li>
-              </ul>
-            </div>
-          </form>
-        </div>
-      </div>
-    </nav>
+  <?php
+    include('navbar.html');
+  ?>
   <!--NAVBAR--END--------------------------------------------------------------------------->
 
   <!--ÜBERSCHRIFT/TEXT--START--------------------------------------------------------------->
