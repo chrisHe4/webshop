78ebffc85cdb2863fe864dfb2de9fbad99ee8745
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-01-19 08:42:56 +0100

    AdminButtons Seite erstellt

---
+++ AdminButtons.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Webshop</title>
+  </head>
+
+  <body>
+  <!--NAVBAR--START--------------------------------------------------------------------------->
+    <?php
+    include('navbar.php');
+    ?>
+  <!--NAVBAR--END--------------------------------------------------------------------------->
+
+  
+    <div class="container">
+      <H1>Adminbereich</H1>
+      <p>Hier könnte noch mehr Text stehen!!!</p>
+    </div>
+    <div class="row row-cols-5 row-pl-sm  ">
+    <button type="submit" class="btn btn-primary btn-lg">Produkte</button>
+    <button type="submit" class="btn btn-primary">Baugruppe</button>
+    <button type="submit" class="btn btn-primary">Maschinentyp</button>
+    <button type="submit" class="btn btn-primary">Konkrete Maschine</button>
+    <button type="submit" class="btn btn-primary">Bedingungen</button>
+    </div>
+
+    
+
+   
+
+
+  <!--ÜBERSCHRIFT/TEXT--STOP--------------------------------------------------------------->
+   <!--script im body nicht im head, weil wenn Fehler in js, dann wird trotzdem html angezeigt-->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
