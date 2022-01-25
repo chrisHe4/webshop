98e0c4818dbb389b64ee0bdef6de2768eee59833
Chris <ef.chemmi@gmail.com>
2022-01-25 10:43:47 +0100

    Impressum und Datenschutz befüllt, Dummy- Daten für die Kontaktinformationen

---
+++ impressum.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Impressum</title>
+  </head>
+
+  <body>
+  <!-- NAVBAR--START------------------------------------------------------------------------- -->
+  <?php
+    include('navbar.php');
+  ?>
+  <!--NAVBAR--END--------------------------------------------------------------------------->
+
+  <!-- Impressum -->
+  <div class="container">
+    <h1>Impressum</h1>
+
+    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+    <p>Dummy-Daten, da die Seite nicht online steht:<br />
+    Max Mustermann<br />
+    Musterladen<br />
+    Musterstra&szlig;e 111<br />
+    Geb&auml;ude 44<br />
+    90210 Musterstadt</p>
+
+    <h2>Kontakt</h2>
+    <p>Telefon: +49 (0) 123 44 55 66<br />
+    Telefax: +49 (0) 123 44 55 99<br />
+    E-Mail: mustermann@musterfirma.de</p>
+
+    <h2>EU-Streitschlichtung</h2>
+    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+    <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+    <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+  </div>
+  </body>
+</html>
