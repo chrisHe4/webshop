b34e45bb5997b0e0008d6dda25f80bcf402316a1
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-02-04 13:17:50 +0100

    aus der Navbar verlingung auf Katalog entfernt

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">   
-            <a class="nav-link active" href="katalog.php">Katalog</a></li>
+            
             <li><a class="nav-link active" href="warenkorb.php">Warenkorb</a></li>
             <li><a class="nav-link active" href="datenschutz.php">Datenschutz</a></li>
             <li><a class="nav-link active" href="impressum.php">Impressum</a> </li>
