10b509764e48d5f674440bbe7cd7713c9659673b
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-02-04 18:21:14 +0100

    in f-Produkt funktion für befüllte Formular geschrieben

--- f_Produkt.php
+++ f_Produkt.php
@@ -19,7 +19,8 @@
       <?php
 $produktID = $_GET["produkt"];
 
-$result = $mysqli->query('SELECT * FROM produkt where produktID ='.$produktID );
+$gewaelteProdukte = $mysqli->query('SELECT * FROM produkt where produktID = '.$produktID );
+$bestimmteProdukt= mysqli_fetch_array($gewaelteProdukte);
 
 
 ?>
@@ -27,44 +28,53 @@ $result = $mysqli->query('SELECT * FROM produkt where produktID ='.$produktID );
 
 
     <!--FORMULAR START---------------------------------------------------------------------->
-    <form>
+    <form action ="f_Produkt.php" method="POST">
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">ProduktID</label>
-          <input type="text" class="form-control" id="inputEmail4"<?php echo $produktID["produktID"]?> >
+          <input type="text" name="produktid" class="form-control" id="inputEmail4"<?php echo $bestimmteProdukt["produktID"]?> >
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Bezeichnung</label>
-          <input type="text" class="form-control" id="inputPassword4">
+          <input type="text" name="bezeichnung" class="form-control" id="inputPassword4">
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Bild</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="bild" class="form-control" id="inputCity">
         </div>
       </div>
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Produktbeschreibung</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea class="form-control" name="produktbeschreibung" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Preis</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="preis" class="form-control" id="inputCity">
         </div>
     <br></br>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-      <button type="submit" class="btn btn-primary">Speichern</button>
+      <button type="submit" name = "speichern"class="btn btn-primary">Speichern</button>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-        <button type="submit" class="btn btn-primary">Löschen</button>
+        <button type="submit" name="löschen" class="btn btn-primary">Löschen</button>
     
     </form>
 
     <!--FORMULAR STOP---------------------------------------------------------------------->
+<?php
+  if(isset($_POST['speichern']))
+  {
+    $produktid= $_POST['produktid'];
+    $bezeichnung= $_POST['bezeichnung'];
+    $bild= $_POST['bild'];
+    $produktbeschreibung= $_POST['produktbeschreibung'];
+    $preis= $_POST['preis'];
 
 
-
+  }
+?>
     
 
    
