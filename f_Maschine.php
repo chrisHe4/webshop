888e02f783cbb52bab0c9e24df160767ed48715e
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-02-05 18:28:46 +0100

    f.Maschine befüllen von formular speichern loschen bearbeiten funktioniert noch nicht ganz

--- f_Maschine.php
+++ f_Maschine.php
@@ -8,39 +8,85 @@
   
     <div class="container">
       <H1>Adminbereich</H1>
-      <p>Hier könnte noch mehr Text stehen!!!</p>
+      
     </div>
+
+    <br></br>
     <!--ÜBERSCHRIFT/TEXT--START---------------------------------------------------------------> 
       <p><b>Maschine bearbeiten</b></p>
    
 
-    
+      <?php
+      $maschinentypID= "";
+      $bezeichnung= "";
+      
+      if(isset($_GET["maschinentyp"]))
+      {
+$maschinentypID = $_GET["maschinentyp"];
+
+$gewaelteMaschinen = $mysqli->query('SELECT * FROM maschinentyp where maschinentypID = '.$maschinentypID );
+$bestimmteMaschine= mysqli_fetch_array($gewaelteMaschinen);
+$maschinentypID= $bestimmteMaschine["maschinentypID"];
+$bezeichnung= $bestimmteMaschine["bezeichnung"];
+
+      }
+
+?>
      
 
 
     <!--FORMULAR START---------------------------------------------------------------------->
-    <form>
+    <form action ="f_Maschine.php" method="POST">
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">MaschinentypID</label>
-          <input type="text" class="form-control" id="inputEmail4">
+          <input type="text" name="maschinentypID" class="form-control" id="inputEmail4" value =<?php echo $maschinentypID?>>
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Bezeichnung</label>
-          <input type="text" class="form-control" id="inputPassword4">
+          <input type="text" name="bezeichnung" class="form-control" id="inputPassword4"value =<?php echo $bezeichnung?>>
         </div>
       </div>
       
     <br></br>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-      <button type="submit" class="btn btn-primary">Speichern</button>
+      <button type="submit"name = "speichern" class="btn btn-primary">Speichern</button>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-        <button type="submit" class="btn btn-primary">Löschen</button>
+        <button type="submit" name="löschen" class="btn btn-primary">Löschen</button>
     
     </form>
 
     <!--FORMULAR STOP---------------------------------------------------------------------->
+    <?php
+  if(isset($_POST['speichern']))
+  {
+    $maschinentypID= intval($_POST['maschinentypID']);
+    $bezeichnung= $_POST['bezeichnung'];
+   ;
+    $maschinecheck = $mysqli->query('SELECT maschinentypID FROM maschinentyp WHERE maschinentypID='.$maschinentypID);
+    if (mysqli_num_rows($maschinecheck)) 
+    {
+      //Update
+      $updateMaschine = $mysqli->query('UPDATE maschinentyp SET bezeichnung = "'.$bezeichnung.'" WHERE maschinentypID = '.$maschinentypID);
+    }
+    else
+    {
+      // Neues Produkt
+      $neueMaschine = $mysqli->query('INSERT INTO maschinentyp (maschinentypID, bezeichnung)VALUES ('.
+      $maschinentypID.',"'.$bezeichnung.'"');
+     
+    }
+    echo "Daten in die DB gespeichert"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
 
+  if(isset($_POST['löschen']))
+  {
+    $maschinentypID= intval($_POST['maschinentypID']);
+    $zuLoeschendeMaschine = $mysqli->query('DELETE FROM maschinentyp WHERE maschinentypID = '.$maschinentypID);
+    
+    echo "Daten aus der DB gelöscht"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
+?>
 
 
     
