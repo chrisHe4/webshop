0589932bb1a5491ff867654c3fd3e558f2d5761f
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-02-06 17:15:50 +0100

    t-maschine url für maschinentyp angepasst, f-Baugruppe: formular befüllen neue baugruppe bearbeiten und loschen

--- f_Baugruppe.php
+++ f_Baugruppe.php
@@ -8,7 +8,7 @@
   
     <div class="container">
       <H1>Adminbereich</H1>
-      <p>Hier könnte noch mehr Text stehen!!!</p>
+      
     </div>
     <!--ÜBERSCHRIFT/TEXT--START---------------------------------------------------------------> 
       <p><b>Baugruppe bearbeiten</b></p>
@@ -16,37 +16,56 @@
 
     
      
+      <?php
+      $baugruppeID= "";
+      $bezeichnung= "";
+      $maschinentyp= "";
+      $produkt= "";
+      
+      if(isset($_GET["baugruppe"]))
+      {
+$baugruppeID = $_GET["baugruppe"];
 
+$gewaelteBaugruppen = $mysqli->query('SELECT * FROM baugruppe where baugruppeID = '.$baugruppeID );
+$bestimmteBaugruppe= mysqli_fetch_array($gewaelteBaugruppen);
+$baugruppeID= $bestimmteBaugruppe["baugruppeID"];
+$bezeichnung= $bestimmteBaugruppe["bezeichnung"];
+$maschinentyp= $bestimmteBaugruppe["maschinentyp"]; 
+$produkt= $bestimmteBaugruppe["produkt"];
+
+      }
+
+?>
 
     <!--FORMULAR START---------------------------------------------------------------------->
-    <form>
+    <form action ="f_Baugruppe.php" method="POST">
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">BaugruppeID</label>
-          <input type="text" class="form-control" id="inputEmail4">
+          <input type="text" name ="baugruppeID" class="form-control" id="inputEmail4" value =<?php echo $baugruppeID?>>
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Bezeichnung</label>
-          <input type="text" class="form-control" id="inputPassword4">
+          <input type="text" name="bezeichnung" class="form-control" id="inputPassword4" value=<?php echo $bezeichnung?>>
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Maschinentyp</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="maschinentyp" class="form-control" id="inputCity"value=<?php echo $maschinentyp?>>
         </div>
       </div>
       
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Produkt</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="produkt" class="form-control" id="inputCity" value=<?php echo $produkt?>>
         </div>
     <br></br>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-      <button type="submit" class="btn btn-primary">Speichern</button>
+      <button type="submit" name = "speichern" class="btn btn-primary">Speichern</button>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-        <button type="submit" class="btn btn-primary">Löschen</button>
+        <button type="submit" name="löschen" class="btn btn-primary">Löschen</button>
     
     </form>
 
@@ -56,7 +75,37 @@
 
     
 
-   
+    <?php
+  if(isset($_POST['speichern']))
+  {
+    $baugruppeID= intval($_POST['baugruppeID']);
+    $bezeichnung= $_POST['bezeichnung'];
+    $maschinentyp=intval( $_POST['maschinentyp']);
+    $produkt= intval($_POST['produkt']);
+    $baugruppecheck = $mysqli->query('SELECT baugruppeID FROM baugruppe WHERE baugruppeID='.$baugruppeID);
+    if (mysqli_num_rows($baugruppecheck)) 
+    {
+      //Update
+      $updateBaugruppe = $mysqli->query('UPDATE baugruppe SET bezeichnung = "'.$bezeichnung.'", maschinentyp = '.$maschinentyp.',produkt = '.$produkt.' WHERE baugruppeID = '.$baugruppeID);
+    }
+    else
+    {
+      // Neue Baugruppe
+      $neueBaugruppe = $mysqli->query('INSERT INTO baugruppe (baugruppeID, bezeichnung, maschinentyp, produkt)VALUES ('.
+        $baugruppeID.',"'.$bezeichnung.'",'.$maschinentyp.','.$produkt.'');
+     
+    }
+    echo "Daten in die DB gespeichert"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
+
+  if(isset($_POST['löschen']))
+  {
+    $baugruppeID= intval($_POST['baugruppeID']);
+    $zuLoeschendeBaugruppe = $mysqli->query('DELETE FROM baugruppe WHERE baugruppeID = '.$baugruppeID);
+    // TODO testen
+    echo "Daten aus der DB gelöscht"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
+?>
 
 
   <!--ÜBERSCHRIFT/TEXT--STOP--------------------------------------------------------------->
--- t_Maschinen.php
+++ t_Maschinen.php
@@ -44,8 +44,8 @@ $maschinentypen = $result->fetch_all(MYSQLI_ASSOC);
           foreach($maschinentypen as $maschinentyp) {
           ?>
           <tr>
-            <td ><?php echo "<a href='f_Maschine.php?produkt=".$maschinentyp["maschinentypID"]."'>".$maschinentyp["maschinentypID"]."</a>" ?></td>
-            <td><?php echo "<a href='f_Maschine.php?produkt=".$maschinentyp["maschinentypID"]."'>".$maschinentyp["bezeichnung"]."</a>" ?></td>
+            <td ><?php echo "<a href='f_Maschine.php?maschinentyp=".$maschinentyp["maschinentypID"]."'>".$maschinentyp["maschinentypID"]."</a>" ?></td>
+            <td><?php echo "<a href='f_Maschine.php?maschinentyp=".$maschinentyp["maschinentypID"]."'>".$maschinentyp["bezeichnung"]."</a>" ?></td>
            
           </tr>
           <?php } ?>
