ee54f49b3daf1644482e7e4fe7536278518eda14
Your NameKati5555 <katarzynamichalska2289@gmail.com>
2022-02-06 23:08:33 +0100

    f-Bedingung funktion befüllen löschen, bearbeiten, neu hinzufügen

--- f_Bedingungen.php
+++ f_Bedingungen.php
@@ -8,7 +8,7 @@
   
     <div class="container">
       <H1>Adminbereich</H1>
-      <p>Hier könnte noch mehr Text stehen!!!</p>
+      
     </div>
     <!--ÜBERSCHRIFT/TEXT--START---------------------------------------------------------------> 
       <p><b>Bedingungen bearbeiten</b></p>
@@ -16,36 +16,55 @@
 
     
      
+      <?php
+      $bedingungID= "";
+      $baugruppe= "";
+      $fertige_baugruppe= "";
+      $menge= "";
+      
+      if(isset($_GET["bedingung"]))
+      {
+$bedingungID = $_GET["bedingung"];
 
+$gewaelteBedingungen = $mysqli->query('SELECT * FROM bedingung where bedingungID = '.$bedingungID );
+$bestimmteBedingung= mysqli_fetch_array($gewaelteBedingungen);
+$bedingungID= $bestimmteBedingung["bedingungID"];
+$baugruppe= $bestimmteBedingung["baugruppe"];
+$fertige_baugruppe= $bestimmteBedingung["fertige_baugruppe"]; 
+$menge= $bestimmteBedingung["menge"];
+
+      }
+
+?>
 
     <!--FORMULAR START---------------------------------------------------------------------->
     <form>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputEmail4">BedingungID</label>
-          <input type="text" class="form-control" id="inputEmail4">
+          <input type="text" name="bedingungID" class="form-control" id="inputEmail4" value =<?php echo $bedingungID?>>
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Baugruppe</label>
-          <input type="text" class="form-control" id="inputPassword4">
+          <input type="text" name="baugruppe" class="form-control" id="inputPassword4" value =<?php echo $baugruppe?>>
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Fertige Baugruppe</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="fertigeBaugruppe" class="form-control" id="inputCity" value =<?php echo $fertige_baugruppe?>>
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">Menge</label>
-          <input type="text" class="form-control" id="inputCity">
+          <input type="text" name="menge" class="form-control" id="inputCity" value =<?php echo $menge?>>
         </div>
     <br></br>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-      <button type="submit" class="btn btn-primary">Speichern</button>
+      <button type="submit"name = "speichern" class="btn btn-primary">Speichern</button>
     <!--BUTTON SPEICHER/LÖSCHEN START------------------------------------------------------->
-        <button type="submit" class="btn btn-primary">Löschen</button>
+        <button type="submit"name="löschen" class="btn btn-primary">Löschen</button>
     
     </form>
 
@@ -56,7 +75,38 @@
     
 
    
+    <?php
+  if(isset($_POST['speichern']))
+  {
+    $bedingungID= intval($_POST['bedingungID']);
+    $baugruppe= intval($_POST['baugruppe']);
+    $fertige_baugruppe=intval( $_POST['fertige_baugruppe']);
+    $menge=intval( $_POST['menge']);
+    
+    $bedingungcheck = $mysqli->query('SELECT bedingungID FROM bedingung WHERE bedingungID='.$bedingungID);
+    if (mysqli_num_rows($bedingungcheck)) 
+    {
+      //Update
+      $updateBedingung = $mysqli->query('UPDATE bedingung SET baugruppe = '.$baugruppe.', fertige_baugruppe = '.$fertige_baugruppe.',menge = '.$menge.' WHERE bedingungID = '.$bedingungID);
+    }
+    else
+    {
+      // Neue Bedingung
+      $neueBedingung = $mysqli->query('INSERT INTO bedingung (bedingungID, baugruppe, fertige_baugruppe, menge)VALUES ('.
+      $bedingungID.','.$baugruppe.','.$fertige_baugruppe.','.$menge.'');
+     
+    }
+    echo "Daten in die DB gespeichert"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
 
+  if(isset($_POST['löschen']))
+  {
+    $bedingungID= intval($_POST['bedingungID']);
+    $zuLoescheBedingung = $mysqli->query('DELETE FROM bedingung WHERE bedingungID = '.$bedingungID);
+    // TODO testen
+    echo "Daten aus der DB gelöscht"; // Optionales TODO Prüfen ob die Anfrage an die DB wirklich geklappt hat
+  }
+?>
 
   <!--ÜBERSCHRIFT/TEXT--STOP--------------------------------------------------------------->
 
